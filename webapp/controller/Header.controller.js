ffdd76b316b67c1bf1ea8b3888721f8adb7cc889
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2021-12-29 02:59:04 -0300

    .

---
+++ webapp/controller/Header.controller.js
@@ -0,0 +1,15 @@
+/**
+ * Controller para o Carrinho
+ */
+sap.ui.define([
+    "vcd94xt10z/app1/fioriapp1/controller/BaseController"
+],
+function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("vcd94xt10z.app1.fioriapp1.controller.Header", {
+        onInit: function () {
+            console.log("Header controller");
+        }
+    });
+});
