f30a4805875d69b346acd30f71809c308975a92f
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2021-12-29 13:05:40 -0300

    .

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -3,23 +3,35 @@
  */
 sap.ui.define([
     "vcd94xt10z/app1/fioriapp1/controller/BaseController",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
 ],
-    function (BaseController,JSONModel) {
+    function (BaseController,JSONModel,Filter,FilterOperator) {
         "use strict";
 
         return BaseController.extend("vcd94xt10z.app1.fioriapp1.controller.Home", {
             onInit: function () {
                 console.log("Home controller");
 
+                // Lista de produtos em formato JSON Array
                 var productList = this.getOwnerComponent().getModel("productList").getData();
-                console.log(productList);
-
+                
+                // criando model
                 var oViewModel = new JSONModel({
                     productList: productList
                 });
+
                 this.getView().setModel(oViewModel, "view");
 
+                // filtrando
+                var aFilter = [
+                    new Filter("price", "GT", 4000)
+                ];
+               
+                var oList = this.byId("gridList");
+    			var oBinding = oList.getBinding("items");
+			    oBinding.filter(aFilter);
             }
         });
     });
