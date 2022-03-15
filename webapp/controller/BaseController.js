82a6a6a007d72a9405640e8a85026b491d62ed43
Vinicius Cesar Dias <dias.viniciuscesar@gmail.com>
2022-03-15 17:48:25 -0300

    .

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -5,12 +5,14 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
     "sap/ui/core/routing/History",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
     "../model/formatter"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,UIComponent,History,formatter) {
+    function (Controller,UIComponent,History,JSONModel,MessageBox,formatter) {
         "use strict";
 
         return Controller.extend("vcd94xt10z.app1.fioriapp1.controller.BaseController", {
@@ -18,6 +20,42 @@ sap.ui.define([
             
             onInit: function () {
                 console.log("Base controller");
+            },
+
+            onComprar: function(evt){
+                var productid = evt.getSource().data("productid");
+                this.addItemToBasket(productid);
+                var basket = this.getBasketObject();
+                console.log(basket);
+
+                MessageBox.success("Item adicionado");
+            },
+
+            addItemToBasket: function(productid){
+                var basket = this.getBasketObject();
+                basket.itemList.push(productid);
+                this.setBasketObject(basket);
+            },
+
+            setBasketObject: function(basket){
+                var oModel = this.getOwnerComponent().getModel("basket");
+                if(!oModel){
+                    oModel = new JSONModel({"basket":null});
+                }
+                oModel.setData(basket,"basket");
+                this.getOwnerComponent().setModel(oModel,"basket");
+            },
+
+            getBasketObject: function(){
+                var oModel = this.getOwnerComponent().getModel("basket");
+                if(!oModel){
+                    var obj = {
+                        total: 0,
+                        itemList: []
+                    };
+                    return obj;
+                }
+                return oModel.getData();
             },
 
             /**
